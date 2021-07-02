6d93a9d0bde432cd9e93adf0578b0e3016bc9856
ansky93 <86812404+ansky93@users.noreply.github.com>
2021-07-02 08:52:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    introTitle: 'Bunny của anh!',
+    introDesc: 'Nếu mỗi ngày của anh có 24 giờ thì anh sẽ xin ông trời thêm 3000 giờ chỉ để ở cạnh em'
+  ,
+    btnIntro: 'Hehehe',
+    title: 'Đây là câu hỏi khảo sát',
+    desc: 'Có phải em đã thích anh từ lần đầu tiên đúng hông? ',
+    btnYes: 'Chuẩn khỏi cần chỉnh',
+    btnNo: 'Không, anh sai rồi',
+    question:'Em ăn sáng chưa ?',
+    btnReply: '',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+ 
